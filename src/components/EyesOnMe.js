b7640c2b29a19a5d08cb8ac047b7d598b142a9fc
fhidesjoseph <fhidesjoseph@gmail.com>
2022-07-14 23:05:10 +0300

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+    const handleFocus=(event)=>{
+        event.preventDefault();
+        console.log('Good!')
+    }
+    const handleBlur=(event)=>{
+        event.preventDefault();
+        console.log('Hey! Eyes on me!');
+    }
+  return (
+
+    <div>
+        <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+    </div>
+  )
+}
+
+export default EyesOnMe 
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,23 @@
 // Code Keypad Component Here
+import React from 'react'
+
+function Keypad() {
+    const handleAuth=(event)=>{
+        event.preventDefault()
+        console.log("Entering password...");
+
+    }
+  return (
+    <div>
+    <label >Passcode
+        <input onChange={handleAuth}
+        id="passcode"
+        type="password"
+        placeholder='****'
+        />
+        </label>
+    </div>
+  )
+}
+
+export default Keypad 
